db9e9d82b3fd9bb5b551624b4daae45ec51d45cf
Ralph Ankele <rankele@brave.com>
2024-05-28 16:46:21 +0200

    initial version of end2end example

---
+++ end2end-example/client/src/main.rs
@@ -0,0 +1,46 @@
+use boomerang::{
+    client::UKeyPair, client::IssuanceC, client::CollectionC,// client::SpendVerifyC,
+    server::IssuanceS,
+};
+use rand_core::OsRng;
+use serde_json::json;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use t256::Config;   // use arksecp256r1
+
+#[tokio::main]
+async fn main() {
+
+    // Generate user keys
+    let ckp = UKeyPair::<Config>::generate(&mut OsRng);
+
+    // start issuance protocol
+    let issuance_m1 = IssuanceC::<Config>::generate_issuance_m1(ckp.clone(), &mut OsRng);
+
+    // serialize issuance_m1 as json string
+    let mut issuance_m1_bytes = Vec::new();
+    issuance_m1.serialize_compressed(&mut issuance_m1_bytes).unwrap();
+    let issuance_m1_json = json!(issuance_m1_bytes).to_string();
+
+    // send issuance_m1 as json string to server
+    println!("Send issuance_m1 to server");
+    let client = reqwest::Client::new();
+    let response = client
+        .post("http://localhost:8080/boomerang_issuance_m2")
+        .header("Content-Type", "application/json")
+        .body(issuance_m1_json)
+        .send()
+        .await;
+
+    let response_body = response.unwrap().text().await;
+    //println!("Response body:\n{}", response_body.unwrap());
+
+    // deserialize issuance_m2 response from server
+    println!("deserialize response");
+    let issuance_m2_bytes: Vec<u8> = serde_json::from_str(&response_body.unwrap()).unwrap();
+    let issuance_m2 = IssuanceS::<Config>::deserialize_compressed(&*issuance_m2_bytes).unwrap();
+
+    println!("check some properties");
+    assert!(issuance_m2.m2.verifying_key.is_on_curve());
+    assert!(issuance_m2.m2.tag_key.is_on_curve());
+    println!("fin");
+}
---
+++ end2end-example/server/src/main.rs
@@ -0,0 +1,59 @@
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use serde_json::json;
+use std::sync::Arc;
+use boomerang::{client::IssuanceC, server::{IssuanceS, ServerKeyPair}};
+use t256::Config;
+use rand_core::OsRng;
+
+async fn boomerang_issuance_m2(data: web::Data<Arc<AppState>>, req_body: String) -> impl Responder {
+    println!("Boomerang Issunance M2...");
+    // Deserialize issuance_m1 from client
+    let issuance_m1_bytes: Vec<u8> = serde_json::from_str(&req_body).unwrap();
+    let issuance_m1 = IssuanceC::<Config>::deserialize_compressed(&*issuance_m1_bytes).unwrap();
+
+    let issuance_m2 = IssuanceS::<Config>::generate_issuance_m2(
+        issuance_m1, 
+        data.skp.clone(), 
+        &mut OsRng);
+
+    // Serialize issuance_m2
+    let mut issuance_m2_bytes = Vec::new();
+    issuance_m2.serialize_compressed(&mut issuance_m2_bytes).unwrap();
+    let issuance_m2_json = json!(issuance_m2_bytes).to_string();
+
+    // send response
+    println!("Boomerang Issuance M2 - Send response...");
+    HttpResponse::Ok().body(issuance_m2_json)
+}
+
+// Shared state struct between all routes
+struct AppState {
+    skp: ServerKeyPair::<Config>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    println!("Initializing server...");
+
+    // Generate server key pair
+    let skp = ServerKeyPair::<Config>::generate(&mut OsRng);
+    let app_state = Arc::new(AppState {
+        skp: skp,
+    });
+
+    println!("Start HTTP server. Wait for requests...");
+    // Start the HTTP server
+    HttpServer::new(move || {
+        App::new()
+            // share the app state between the routes
+            .app_data(web::Data::new(app_state.clone()))
+            // configure routes
+            .route("/boomerang_issuance_m2", web::post().to(boomerang_issuance_m2))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
