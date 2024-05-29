295dd85040d21949a3695ecc2af38b719e16f985
Ralph Ankele <rankele@brave.com>
2024-05-29 17:33:14 +0200

    implement entire issuance flow end2end

--- end2end-example/client/src/main.rs
+++ end2end-example/client/src/main.rs
@@ -1,35 +1,32 @@
+use acl::verify::SigVerify;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use boomerang::{
     client::{IssuanceC, UKeyPair},
     //client::{CollectionC, SpendVerifyC},
-    server::IssuanceS,
+    server::{IssuanceS, ServerKeyPair},
 };
 use rand::rngs::OsRng;
 use serde_json::json;
 use t256::Config; // use arksecp256r1
 
-#[tokio::main]
-async fn main() {
-    // Generate user keys
-    let ckp = UKeyPair::<Config>::generate(&mut OsRng);
-
-    // start issuance protocol
-    let issuance_m1 = IssuanceC::<Config>::generate_issuance_m1(ckp.clone(), &mut OsRng);
-
+async fn send_message_to_server_and_await_response(
+    issuance_c: IssuanceC<Config>,
+    endpoint: String,
+) -> IssuanceS<Config> {
     // serialize issuance_m1 as json string
-    let mut issuance_m1_bytes = Vec::new();
-    issuance_m1
-        .serialize_compressed(&mut issuance_m1_bytes)
+    let mut issuance_c_bytes = Vec::new();
+    issuance_c
+        .serialize_compressed(&mut issuance_c_bytes)
         .unwrap();
-    let issuance_m1_json = json!(issuance_m1_bytes).to_string();
+    let issuance_c_json = json!(issuance_c_bytes).to_string();
 
     // send issuance_m1 as json string to server
     println!("Send issuance_m1 to server");
     let client = reqwest::Client::new();
     let response = client
-        .post("http://localhost:8080/boomerang_issuance_m2")
+        .post(endpoint)
         .header("Content-Type", "application/json")
-        .body(issuance_m1_json)
+        .body(issuance_c_json)
         .send()
         .await;
 
@@ -38,11 +35,84 @@ async fn main() {
 
     // deserialize issuance_m2 response from server
     println!("deserialize response");
-    let issuance_m2_bytes: Vec<u8> = serde_json::from_str(&response_body.unwrap()).unwrap();
-    let issuance_m2 = IssuanceS::<Config>::deserialize_compressed(&*issuance_m2_bytes).unwrap();
+    let issuance_s_bytes: Vec<u8> = serde_json::from_str(&response_body.unwrap()).unwrap();
+    let issuance_s = IssuanceS::<Config>::deserialize_compressed(&*issuance_s_bytes).unwrap();
 
-    println!("check some properties");
-    assert!(issuance_m2.m2.verifying_key.is_on_curve());
-    assert!(issuance_m2.m2.tag_key.is_on_curve());
-    println!("fin");
+    issuance_s
+}
+
+async fn get_server_keypair_from_server() -> ServerKeyPair<Config> {
+    let client = reqwest::Client::new();
+    let response = client
+    .get("http://localhost:8080/server_keypair")
+    .send()
+    .await;
+
+    let response_body = response.unwrap().text().await;
+
+    // deserialize server keypair
+    let server_keypair_bytes: Vec<u8> = serde_json::from_str(&response_body.unwrap()).unwrap();
+    let server_keypair = ServerKeyPair::<Config>::deserialize_compressed(&*server_keypair_bytes).unwrap();
+    server_keypair
+}
+
+#[tokio::main]
+async fn main() {
+    // Generate user keys
+    let ckp = UKeyPair::<Config>::generate(&mut OsRng);
+
+    // Get server key from server
+    let skp = get_server_keypair_from_server().await;
+
+    // start issuance protocol
+    let issuance_state = {
+        // issuance m1
+        let issuance_m1 = IssuanceC::<Config>::generate_issuance_m1(
+            ckp.clone(), 
+            &mut OsRng,
+        );
+
+        // send to server get m2
+        let issuance_m2 = send_message_to_server_and_await_response(
+            issuance_m1.clone(), 
+            "http://localhost:8080/boomerang_issuance_m2".to_string(),
+        ).await;
+        // check some properties
+        println!("check some properties");
+        assert!(issuance_m2.m2.verifying_key.is_on_curve());
+        assert!(issuance_m2.m2.tag_key.is_on_curve());
+        println!("fin");
+
+        // issuance m3
+        let issuance_m3 = IssuanceC::<Config>::generate_issuance_m3(
+            issuance_m1.clone(), 
+            issuance_m2, 
+            &mut OsRng);
+        
+        // send to server get m4
+        let issuance_m4 = send_message_to_server_and_await_response(
+            issuance_m3.clone(), 
+            "http://localhost:8080/boomerang_issuance_m4".to_string(),
+        ).await;
+
+        // populate state
+        let issuance_state = IssuanceC::<Config>::populate_state(
+            issuance_m3,
+            issuance_m4, 
+            skp.clone(), 
+            ckp.clone(),
+        );
+
+        let sig = &issuance_state.sig_state[0];
+
+        let check = SigVerify::<Config>::verify(
+            skp.s_key_pair.verifying_key,
+            skp.s_key_pair.tag_key,
+            sig.clone(),
+            "message"
+        );
+        assert!(check == true);
+        println!("Signature check passed!");
+        issuance_state
+    };
 }
--- end2end-example/server/src/main.rs
+++ end2end-example/server/src/main.rs
@@ -6,8 +6,34 @@ use boomerang::{
 };
 use rand::rngs::OsRng;
 use serde_json::json;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use t256::Config;
+
+async fn boomerang_issuance_m4(data: web::Data<Arc<AppState>>, req_body: String) -> impl Responder {
+    println!("Boomerang Issunance M4...");
+    // Deserialize issuance_m1 from client
+    let issuance_m3_bytes: Vec<u8> = serde_json::from_str(&req_body).unwrap();
+    let issuance_m3 = IssuanceC::<Config>::deserialize_compressed(&*issuance_m3_bytes).unwrap();
+
+    let issuance_s = data.issuance_s.lock().unwrap().clone();
+
+    let issuance_m4 = IssuanceS::<Config>::generate_issuance_m4(
+        issuance_m3,
+        issuance_s.unwrap(),
+        data.skp.clone(),
+    );
+
+    // Serialize issuance_m4
+    let mut issuance_m4_bytes = Vec::new();
+    issuance_m4
+        .serialize_compressed(&mut issuance_m4_bytes)
+        .unwrap();
+    let issuance_m4_json = json!(issuance_m4_bytes).to_string();
+
+    // send response
+    println!("Boomerang Issuance M4 - Send response...");
+    HttpResponse::Ok().body(issuance_m4_json)
+}
 
 async fn boomerang_issuance_m2(data: web::Data<Arc<AppState>>, req_body: String) -> impl Responder {
     println!("Boomerang Issunance M2...");
@@ -17,6 +43,8 @@ async fn boomerang_issuance_m2(data: web::Data<Arc<AppState>>, req_body: String)
 
     let issuance_m2 =
         IssuanceS::<Config>::generate_issuance_m2(issuance_m1, data.skp.clone(), &mut OsRng);
+    let mut issuance_s = data.issuance_s.lock().unwrap();
+    *issuance_s = Some(issuance_m2.clone());
 
     // Serialize issuance_m2
     let mut issuance_m2_bytes = Vec::new();
@@ -30,9 +58,21 @@ async fn boomerang_issuance_m2(data: web::Data<Arc<AppState>>, req_body: String)
     HttpResponse::Ok().body(issuance_m2_json)
 }
 
+async fn server_keypair(data: web::Data<Arc<AppState>>) -> impl Responder {
+    // Serialize server key pair
+    let mut server_key_bytes: Vec<u8> = Vec::new();
+    data.skp
+        .serialize_compressed(&mut server_key_bytes)
+        .unwrap();
+    let server_key_json = json!(server_key_bytes).to_string();
+
+    HttpResponse::Ok().body(server_key_json)
+}
+
 // Shared state struct between all routes
 struct AppState {
     skp: ServerKeyPair<Config>,
+    issuance_s: Mutex<Option<IssuanceS<Config>>>,
 }
 
 #[actix_web::main]
@@ -41,7 +81,10 @@ async fn main() -> std::io::Result<()> {
 
     // Generate server key pair
     let skp = ServerKeyPair::<Config>::generate(&mut OsRng);
-    let app_state = Arc::new(AppState { skp: skp });
+    let app_state = Arc::new(AppState {
+        skp: skp,
+        issuance_s: Mutex::new(None),
+    });
 
     println!("Start HTTP server. Wait for requests...");
     // Start the HTTP server
@@ -54,6 +97,11 @@ async fn main() -> std::io::Result<()> {
                 "/boomerang_issuance_m2",
                 web::post().to(boomerang_issuance_m2),
             )
+            .route(
+                "/boomerang_issuance_m4",
+                web::post().to(boomerang_issuance_m4),
+            )
+            .route("/server_keypair", web::get().to(server_keypair))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
