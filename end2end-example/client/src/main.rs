30c176c6c61f5624bbaa0f56b2ef4940cdd6835c
Ralph Ankele <rankele@brave.com>
2024-07-02 15:00:56 +0200

    add collection protocol to end2end example

--- end2end-example/client/src/main.rs
+++ end2end-example/client/src/main.rs
@@ -1,15 +1,14 @@
 use acl::verify::SigVerify;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use boomerang::{
-    client::{IssuanceC, UKeyPair},
-    //client::{CollectionC, SpendVerifyC},
-    server::{IssuanceS, ServerKeyPair},
+    client::{CollectionC, IssuanceC, SpendVerifyC, UKeyPair},
+    server::{CollectionS, IssuanceS, ServerKeyPair},
 };
 use rand::rngs::OsRng;
 use serde_json::json;
 use t256::Config; // use arksecp256r1
 
-async fn send_message_to_server_and_await_response(
+async fn issuance_send_message_to_server_and_await_response(
     issuance_c: IssuanceC<Config>,
     endpoint: String,
 ) -> IssuanceS<Config> {
@@ -39,18 +38,62 @@ async fn send_message_to_server_and_await_response(
     IssuanceS::<Config>::deserialize_compressed(&*issuance_s_bytes).unwrap()
 }
 
+async fn collection_send_message_to_server_and_await_response(
+    collection_c: CollectionC<Config>,
+    endpoint: String,
+) -> CollectionS<Config> {
+    // serialize issuance_m* as json string
+    let mut collection_c_bytes = Vec::new();
+    collection_c
+        .serialize_compressed(&mut collection_c_bytes)
+        .unwrap();
+    let collection_c_json = json!(collection_c_bytes).to_string();
+
+    // send collection_m* as json string to server
+    println!("Send collection_m* to server");
+    let client = reqwest::Client::new();
+    let response = client
+        .post(endpoint)
+        .header("Content-Type", "application/json")
+        .body(collection_c_json)
+        .send()
+        .await;
+
+    let response_body = response.unwrap().text().await;
+    //println!("Response body:\n{}", response_body.unwrap());
+
+    // deserialize collection_m2 response from server
+    println!("deserialize response");
+    let collection_s_bytes: Vec<u8> = serde_json::from_str(&response_body.unwrap()).unwrap();
+    CollectionS::<Config>::deserialize_compressed(&*collection_s_bytes).unwrap()
+}
+
 async fn get_server_keypair_from_server() -> ServerKeyPair<Config> {
     let client = reqwest::Client::new();
     let response = client
-    .get("http://localhost:8080/server_keypair")
-    .send()
-    .await;
+        .get("http://localhost:8080/server_keypair")
+        .send()
+        .await;
 
     let response_body = response.unwrap().text().await;
 
     // deserialize server keypair
     let server_keypair_bytes: Vec<u8> = serde_json::from_str(&response_body.unwrap()).unwrap();
     ServerKeyPair::<Config>::deserialize_compressed(&*server_keypair_bytes).unwrap()
+}
+
+async fn get_collection_m1() -> CollectionS<Config> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://localhost:8080/boomerang_collection_m1")
+        .send()
+        .await;
+
+    let response_body = response.unwrap().text().await;
+
+    // deserialize message m1
+    let collection_m1_bytes: Vec<u8> = serde_json::from_str(&response_body.unwrap()).unwrap();
+    CollectionS::<Config>::deserialize_compressed(&*collection_m1_bytes).unwrap()
 }
 
 #[tokio::main]
@@ -64,16 +107,14 @@ async fn main() {
     // start issuance protocol
     let issuance_state = {
         // issuance m1
-        let issuance_m1 = IssuanceC::<Config>::generate_issuance_m1(
-            ckp.clone(), 
-            &mut OsRng,
-        );
+        let issuance_m1 = IssuanceC::<Config>::generate_issuance_m1(ckp.clone(), &mut OsRng);
 
         // send to server get m2
-        let issuance_m2 = send_message_to_server_and_await_response(
-            issuance_m1.clone(), 
+        let issuance_m2 = issuance_send_message_to_server_and_await_response(
+            issuance_m1.clone(),
             "http://localhost:8080/boomerang_issuance_m2".to_string(),
-        ).await;
+        )
+        .await;
         // check some properties
         println!("check some properties");
         assert!(issuance_m2.m2.verifying_key.is_on_curve());
@@ -81,24 +122,19 @@ async fn main() {
         println!("fin");
 
         // issuance m3
-        let issuance_m3 = IssuanceC::<Config>::generate_issuance_m3(
-            issuance_m1.clone(), 
-            issuance_m2, 
-            &mut OsRng);
-        
+        let issuance_m3 =
+            IssuanceC::<Config>::generate_issuance_m3(issuance_m1.clone(), issuance_m2, &mut OsRng);
+
         // send to server get m4
-        let issuance_m4 = send_message_to_server_and_await_response(
-            issuance_m3.clone(), 
+        let issuance_m4 = issuance_send_message_to_server_and_await_response(
+            issuance_m3.clone(),
             "http://localhost:8080/boomerang_issuance_m4".to_string(),
-        ).await;
+        )
+        .await;
 
         // populate state
-        let issuance_state = IssuanceC::<Config>::populate_state(
-            issuance_m3,
-            issuance_m4, 
-            skp.clone(), 
-            ckp.clone(),
-        );
+        let issuance_state =
+            IssuanceC::<Config>::populate_state(issuance_m3, issuance_m4, skp.clone(), ckp.clone());
 
         let sig = &issuance_state.sig_state[0];
 
@@ -106,10 +142,70 @@ async fn main() {
             skp.s_key_pair.verifying_key,
             skp.s_key_pair.tag_key,
             sig.clone(),
-            "message"
+            "message",
         );
         assert!(check);
         println!("Signature check passed!");
         issuance_state
+    };
+
+    // start collection protocol
+    let collection_state = {
+        // collection m1 - request from server
+        let collection_m1 = get_collection_m1().await;
+
+        // collection m2
+        let collection_m2 = CollectionC::<Config>::generate_collection_m2(
+            &mut OsRng,
+            issuance_state,
+            collection_m1.clone(),
+            skp.clone(),
+        );
+        assert!(collection_m2.m2.comm.comm.is_on_curve());
+
+        // send to server get m3
+        let collection_m3 = collection_send_message_to_server_and_await_response(
+            collection_m2.clone(),
+            "http://localhost:8080/boomerang_collection_m3".to_string(),
+        )
+        .await;
+
+        // m4
+        let collection_m4 = CollectionC::<Config>::generate_collection_m4(
+            &mut OsRng,
+            collection_m2.clone(),
+            collection_m3.clone(),
+        );
+
+        // send to server get m5
+        let collection_m5 = collection_send_message_to_server_and_await_response(
+            collection_m4.clone(),
+            "http://localhost:8080/boomerang_collection_m5".to_string(),
+        )
+        .await;
+
+        // populate state
+        let collection_state = CollectionC::<Config>::populate_state(
+            collection_m4.clone(),
+            collection_m5.clone(),
+            skp.clone(),
+            ckp.clone(),
+        );
+        assert!(collection_state.sig_state[0].sigma.zeta.is_on_curve());
+        assert!(collection_state.sig_state[0].sigma.zeta1.is_on_curve());
+
+        // signature
+        let sig_n = &collection_state.sig_state[0];
+
+        let check = SigVerify::<Config>::verify(
+            skp.s_key_pair.verifying_key,
+            skp.s_key_pair.tag_key,
+            sig_n.clone(),
+            "message",
+        );
+        assert!(check);
+        println!("Signature check passed!");
+
+        collection_state
     };
 }
--- end2end-example/server/src/main.rs
+++ end2end-example/server/src/main.rs
@@ -1,19 +1,97 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use ark_ec::models::CurveConfig;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ark_std::One;
 use boomerang::{
-    client::IssuanceC,
-    server::{IssuanceS, ServerKeyPair},
+    client::{CollectionC, IssuanceC},
+    server::{CollectionS, IssuanceS, ServerKeyPair},
 };
 use rand::rngs::OsRng;
 use serde_json::json;
 use std::sync::Mutex;
 use t256::Config;
 
+type SF = <Config as CurveConfig>::ScalarField;
+
+async fn boomerang_collection_m5(data: web::Data<AppState>, req_body: String) -> impl Responder {
+    println!("Boomerang Collection M5...");
+    // Deserialize collection_m4 from client
+    let collection_m4_bytes: Vec<u8> = serde_json::from_str(&req_body).unwrap();
+    let collection_m4 =
+        CollectionC::<Config>::deserialize_compressed(collection_m4_bytes.as_slice()).unwrap();
+
+    let collection_s = data.collection_s.lock().unwrap().clone();
+    let collection_m5 = CollectionS::<Config>::generate_collection_m5(
+        collection_m4,
+        collection_s.unwrap(),
+        data.skp.clone(),
+    );
+
+    // Serialize collection_m3
+    let mut collection_m5_bytes = Vec::new();
+    collection_m5
+        .serialize_compressed(&mut collection_m5_bytes)
+        .unwrap();
+    let collection_m5_json = json!(collection_m5_bytes).to_string();
+
+    // send response
+    println!("Boomerang Collection M5 - Send response...");
+    HttpResponse::Ok().body(collection_m5_json)
+}
+
+async fn boomerang_collection_m3(data: web::Data<AppState>, req_body: String) -> impl Responder {
+    println!("Boomerang Collection M3...");
+    // Deserialize collection_m2 from client
+    let collection_m2_bytes: Vec<u8> = serde_json::from_str(&req_body).unwrap();
+    let collection_m2 =
+        CollectionC::<Config>::deserialize_compressed(collection_m2_bytes.as_slice()).unwrap();
+
+    let v: SF = SF::one();
+    let mut collection_s = data.collection_s.lock().unwrap();
+
+    let collection_m3 = CollectionS::<Config>::generate_collection_m3(
+        &mut OsRng,
+        collection_m2,
+        collection_s.clone().unwrap(),
+        data.skp.clone(),
+        v,
+    );
+    *collection_s = Some(collection_m3.clone());
+
+    // Serialize collection_m3
+    let mut collection_m3_bytes = Vec::new();
+    collection_m3
+        .serialize_compressed(&mut collection_m3_bytes)
+        .unwrap();
+    let collection_m3_json = json!(collection_m3_bytes).to_string();
+
+    // send response
+    println!("Boomerang Collection M3 - Send response...");
+    HttpResponse::Ok().body(collection_m3_json)
+}
+
+async fn boomerang_collection_m1(data: web::Data<AppState>) -> impl Responder {
+    let collection_m1 = CollectionS::<Config>::generate_collection_m1(&mut OsRng);
+
+    let mut collection_s = data.collection_s.lock().unwrap();
+    *collection_s = Some(collection_m1.clone());
+
+    // Serialize server key pair
+    let mut collection_m1_bytes = Vec::new();
+    collection_m1
+        .serialize_compressed(&mut collection_m1_bytes)
+        .unwrap();
+    let collection_m1_json = json!(collection_m1_bytes).to_string();
+
+    HttpResponse::Ok().body(collection_m1_json)
+}
+
 async fn boomerang_issuance_m4(data: web::Data<AppState>, req_body: String) -> impl Responder {
     println!("Boomerang Issuance M4...");
     // Deserialize issuance_m1 from client
     let issuance_m3_bytes: Vec<u8> = serde_json::from_str(&req_body).unwrap();
-    let issuance_m3 = IssuanceC::<Config>::deserialize_compressed(issuance_m3_bytes.as_slice()).unwrap();
+    let issuance_m3 =
+        IssuanceC::<Config>::deserialize_compressed(issuance_m3_bytes.as_slice()).unwrap();
 
     let issuance_s = data.issuance_s.lock().unwrap().clone();
 
@@ -73,6 +151,7 @@ async fn server_keypair(data: web::Data<AppState>) -> impl Responder {
 struct AppState {
     skp: ServerKeyPair<Config>,
     issuance_s: Mutex<Option<IssuanceS<Config>>>,
+    collection_s: Mutex<Option<CollectionS<Config>>>,
 }
 
 #[actix_web::main]
@@ -83,6 +162,7 @@ async fn main() -> std::io::Result<()> {
     let app_state = web::Data::new(AppState {
         skp: ServerKeyPair::<Config>::generate(&mut OsRng),
         issuance_s: Mutex::new(None),
+        collection_s: Mutex::new(None),
     });
 
     println!("Start HTTP server. Wait for requests...");
@@ -101,6 +181,18 @@ async fn main() -> std::io::Result<()> {
                 web::post().to(boomerang_issuance_m4),
             )
             .route("/server_keypair", web::get().to(server_keypair))
+            .route(
+                "/boomerang_collection_m1",
+                web::get().to(boomerang_collection_m1),
+            )
+            .route(
+                "/boomerang_collection_m3",
+                web::post().to(boomerang_collection_m3),
+            )
+            .route(
+                "/boomerang_collection_m5",
+                web::post().to(boomerang_collection_m5),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
