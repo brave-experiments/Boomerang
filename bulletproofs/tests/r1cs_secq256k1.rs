877fe0a30c3d8f8109dc75ca67c2c2f1b9701ece
Ralph Giles <rgiles@brave.com>
2024-07-26 12:06:32 -0700

    bulletproofs: Fixup too_many_arguments annotation
    This warning in specific to clippy, so the bare directive has
    no effect, but rustc 1.80 warns about it.

--- bulletproofs/tests/r1cs_secq256k1.rs
+++ bulletproofs/tests/r1cs_secq256k1.rs
@@ -229,7 +229,7 @@ fn example_gadget<CS: ConstraintSystem<Fr>>(
 }
 
 // Prover's scope
-#[allow(too_many_arguments)]
+#[allow(clippy::too_many_arguments)]
 fn example_gadget_proof(
     pc_gens: &PedersenGens<Affine>,
     bp_gens: &BulletproofGens<Affine>,
